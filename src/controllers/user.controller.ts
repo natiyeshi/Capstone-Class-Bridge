f5fdb5ea57c2fa87684c795be6ec27af700c53a5
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-04-28 22:01:14 +0300

    ASDF

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import { asyncWrapper, RouteError, sendApiResponse } from "../utils";
 import { db, passwordCrypt, zodErrorFmt } from "../libs";
 import { User } from "@prisma/client";
+import queryValidator from "../validators/query.validator";
 
 export const getUsersController = asyncWrapper(async (req, res) => {
   const users = await db.user.findMany();
@@ -30,6 +31,38 @@ export const getMe = asyncWrapper(async (req, res) => {
 
 
 export const updateUser = asyncWrapper(async (req, res) => {
+  const queryParamValidation = queryValidator
+    .queryParamIDValidator("Teacher ID not provided or invalid.")
+    .safeParse(req.params);
+
+  if(!queryParamValidation.success) {
+    throw RouteError.BadRequest("Invalid ID format.");
+  }
+    
+  const id = queryParamValidation.data.id;
+  if (!id) throw RouteError.BadRequest("User Not Found!");
+
+  const { firstName, lastName, phoneNumber } = req.body;
+
+  if (!firstName || !lastName || !phoneNumber) {
+    throw RouteError.BadRequest("Missing required fields: firstName, lastName, or phoneNumber");
+  }
+
+  const updatedUser = await db.user.update({
+    where: { id },
+    data: { firstName, lastName, phoneNumber },
+  });
+
+  return sendApiResponse({
+    res,
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "User updated successfully",
+    result: updatedUser,
+  });
+});
+
+export const updateMe = asyncWrapper(async (req, res) => {
   const id = req.user?._id ?? null;
   if (!id) throw RouteError.BadRequest("User Not Found!");
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import middleware from "../middleware";
-import { getUsersController,getMe, updateUser } from "../controllers/user.controller"
+import { getUsersController,getMe, updateUser, updateMe } from "../controllers/user.controller"
 import Auth from "../middleware/auth";
 const router = Router();
 
 router.get("/", getUsersController);
 router.get("/get-me",Auth.authenticationMiddleWare, getMe);
-router.put("/update",Auth.authenticationMiddleWare, updateUser);
+router.put("/update/:id", updateUser);
+router.put("/update-me",Auth.authenticationMiddleWare, updateMe);
 
 
 export default router;
